5abc9365b15fd1695702e90b176a19da07ebe85e
16bitash <entropy.ash@gmail.com>
2023-02-20 22:55:46 +0530

    Complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,34 +3,49 @@ import "../styles/App.css";
 import { movies } from "../utils/movieList";
 
 const App = () => {
-    const [inputValue, setInputValue] = useState("");
-    const [results, setResult] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [results, setResult] = useState(null);
 
-    function handleClick(e) {}
+  function handleClick(e) {}
 
-    function handleChange(e) {}
+  function handleChange(event) {
+    const latestInputValue = event.target.value;
+    setInputValue(latestInputValue);
 
-    return (
-        <div id="main">
-            <form id="form">
-                <input
-                    type="text"
-                    onChange={handleChange}
-                    value={inputValue}
-                    id="movie-input"
-                ></input>
-                <button id="button" onClick={handleClick}>
-                    Search
-                </button>
-            </form>
-            <div id="result">
-                {results &&
-                    results.map((movie) => (
-                        <div className="movie">{movie}</div>
-                    ))}
-            </div>
-        </div>
-    );
+    if (latestInputValue === "") {
+        setResult(null);
+        return;
+    };
+
+    const searchRegex = new RegExp(latestInputValue, "i");
+
+    const searchFilteredMovies = movies.filter((movie) => {
+      const doesMatchSearch = searchRegex.test(movie.title);
+      return doesMatchSearch;
+    });
+
+    setResult(searchFilteredMovies);
+  }
+
+  return (
+    <div id="main">
+      <form id="form">
+        <input
+          type="text"
+          onChange={handleChange}
+          value={inputValue}
+          id="movie-input"
+        ></input>
+        <button id="button" onClick={handleClick}>
+          Search
+        </button>
+      </form>
+      <div id="result">
+        {results &&
+          results.map((movie) => <div className="movie" key={movie.id}>{movie.title}</div>)}
+      </div>
+    </div>
+  );
 };
 
 export default App;
