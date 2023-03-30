c2608ff79ef5bc9ec960096af216de4009959952
imchavdatushar <120701154+imchavdatushar@users.noreply.github.com>
2023-03-30 11:55:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,49 +3,34 @@ import "../styles/App.css";
 import { movies } from "../utils/movieList";
 
 const App = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [results, setResult] = useState(null);
+    const [inputValue, setInputValue] = useState("");
+    const [results, setResult] = useState(null);
 
-  function handleClick(e) {}
+    function handleClick(e) {}
 
-  function handleChange(event) {
-    const latestInputValue = event.target.value;
-    setInputValue(latestInputValue);
+    function handleChange(e) {}
 
-    if (latestInputValue === "") {
-        setResult(null);
-        return;
-    };
-
-    const searchRegex = new RegExp(latestInputValue, "i");
-
-    const searchFilteredMovies = movies.filter((movie) => {
-      const doesMatchSearch = searchRegex.test(movie.title);
-      return doesMatchSearch;
-    });
-
-    setResult(searchFilteredMovies);
-  }
-
-  return (
-    <div id="main">
-      <form id="form">
-        <input
-          type="text"
-          onChange={handleChange}
-          value={inputValue}
-          id="movie-input"
-        ></input>
-        <button id="button" onClick={handleClick}>
-          Search
-        </button>
-      </form>
-      <div id="result">
-        {results &&
-          results.map((movie) => <div className="movie" key={movie.id}>{movie.title}</div>)}
-      </div>
-    </div>
-  );
+    return (
+        <div id="main">
+            <form id="form">
+                <input
+                    type="text"
+                    onChange={handleChange}
+                    value={inputValue}
+                    id="movie-input"
+                ></input>
+                <button id="button" onClick={handleClick}>
+                    Search
+                </button>
+            </form>
+            <div id="result">
+                {results &&
+                    results.map((movie) => (
+                        <div className="movie">{movie}</div>
+                    ))}
+            </div>
+        </div>
+    );
 };
 
 export default App;
